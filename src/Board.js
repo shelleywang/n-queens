2ce08455d1b59998a3dd5ec3e76df72c456221fe
shelleywang <shelleyw13@gmail.com>
2015-07-06 16:00:06 -0700

    Fixed accidently changing helper function

--- src/Board.js
+++ src/Board.js
@@ -245,7 +245,7 @@
   var makeEmptyMatrix = function(n) {
     return _(_.range(n)).map(function() {
       return _(_.range(n)).map(function() {
-        return rows.length-1;
+        return 0;
       });
     });
   };
